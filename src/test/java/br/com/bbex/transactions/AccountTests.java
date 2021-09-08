d7787dce9825fafaf12553986bead0278265626e
Afonso Lima <ayaher.hayer.inaher@gmail.com>
2021-09-08 19:31:34 -0300

    Tests for Account.

--- src/test/java/br/com/bbex/transactions/AccountTests.java
+++ src/test/java/br/com/bbex/transactions/AccountTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import br.com.bbex.transactions.controller.AccountController;
+import br.com.bbex.transactions.controller.request.CreditLimitRequest;
 import br.com.bbex.transactions.model.Account;
 import br.com.bbex.transactions.repository.AccountRepository;
 import br.com.bbex.transactions.service.AccountService;
@@ -83,6 +84,30 @@ class AccountTests {
 				.andDo(print())
 				.andExpect(status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.document_number").value(save.getDocument_number()));
+	}
+
+	@Test
+	public void setAccountCreditLimitEndpoint() throws Exception {	
+		Account account = new Account("87878787");
+		Account save = accountRepository.save(account);
+
+		// Initial limit must be 0
+		this.mockMvc.perform(get("/api/accounts/{accountId}", save.getAccount_id()))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.availableCreditLimit").value(0));
+
+		CreditLimitRequest newCreditLimit = new CreditLimitRequest(500);
+		this.mockMvc.perform(MockMvcRequestBuilders
+			.patch("/api/accounts/{accountId}/creditlimit", save.getAccount_id())
+			.content(objectMapper.writeValueAsString(newCreditLimit))
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON))
+			.andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.account_id").value(account.getAccount_id()))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.document_number").value(account.getDocument_number()))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.availableCreditLimit").value(newCreditLimit.getCreditLimit()));
 	}
 
 }
